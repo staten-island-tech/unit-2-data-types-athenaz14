a6268ca58c26327bf4df9cff6ac24977d6e4b5b7
athenaz14 <athenaz11@nycstudents.net>
2026-03-10 09:22:10 -0400

    Create Dictionary_Project.py

---
+++ Dictionary_Project.py
@@ -0,0 +1,7 @@
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
+print(best_buy_items[0]["name"])
