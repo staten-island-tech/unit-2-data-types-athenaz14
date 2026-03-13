8416eae2bd601f2dac3e930375ac49eeb8de63ae
athenaz14 <athenaz11@nycstudents.net>
2026-03-13 09:02:41 -0400

    Update Dictionary_Project.py

--- Dictionary_Project.py
+++ Dictionary_Project.py
@@ -3,5 +3,10 @@ item = {
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+
 }
-print(best_buy_items[0]["name"])
+print(item[0]["name"])
