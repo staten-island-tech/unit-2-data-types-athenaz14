2bd063fe56f1eb638073d5c07ee44cf28e52ca83
athenaz14 <athenaz11@nycstudents.net>
2026-03-16 09:24:15 -0400

    Update Dictionary_Project.py

--- Dictionary_Project.py
+++ Dictionary_Project.py
@@ -1,12 +1,14 @@
-item = {
+best_buy_items = {
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."}
+    
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])
 
-}
 print(item[0]["name"])
